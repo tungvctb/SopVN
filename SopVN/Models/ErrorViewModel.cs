3b27f4a65ee718bc538c0e01bdb54db50ffb9977
Nguyen Thanh Tung <ngthtunghhtb@gmail.com>
2024-03-27 15:13:10 +0700

    second commit

---
+++ SopVN/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SopVN.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
